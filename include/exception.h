fb671d8b1ff04ad9a8da908c5da5838e22edded6
Raj Tyagi <rajtyagi2718@gmail.com>
2021-02-18 00:46:48 -0500

    exception: mostly rm. gitignore: deleted prev tracked.

--- include/exception.h
+++ include/exception.h
@@ -4,28 +4,6 @@
 #include <stdexcept>  // exception
 #include <string>     // string
 
-class BoardIndexException : public std::exception
-{
-  public:
-    BoardIndexException(int index);
-    const char* what() const noexcept override;
-
-  private:
-    std::string message;
-    int index;
-};
-
-class SpawnPieceIndexException : public std::exception
-{
-  public:
-    SpawnPieceIndexException(int index);
-    const char* what() const noexcept override;
-
-  private:
-    std::string message;
-    int index;
-};
-
 class GameActionIndexException : public std::exception
 {
   public:
--- src/exception.cpp
+++ src/exception.cpp
@@ -2,35 +2,10 @@
 #include <sstream>                 // ostringstream
 #include <string>                  // to_string
 
-BoardIndexException::BoardIndexException(int index)
-  : index{index}
-{
-  message = "move direc must be either: 0, 1, 2, 3.";
-  message += " given direc of " + std::to_string(index) + '.';
-
-}
-
-const char* BoardIndexException::what() const noexcept
-{
-  return message.c_str(); 
-}
-
-SpawnPieceIndexException::SpawnPieceIndexException(int index)
-  : message{"generated spawn index must be integer between [0, 6]."}, 
+GameActionIndexException::GameActionIndexException(int index)
+  : message{"agent action must be integer between [0, 4]."},
     index{index}
 {
-  message += " given index " + std::to_string(index) + '.';
-}
-
-const char* SpawnPieceIndexException::what() const noexcept
-{
-  return message.c_str(); 
-}
-
-GameActionIndexException::GameActionIndexException(int index)
-  : index{index}
-{
-  message = "agent action must be integer between [0, 4].";
   message += " given action of " + std::to_string(index) + '.';
 
 }
